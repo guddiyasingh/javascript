af6c4e1fa946d729de7be59449ad3d1d6752ebd2
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-05-07 03:58:03 +0530

    add object

--- new.js
+++ new.js
@@ -31,3 +31,66 @@ let r = null;
 
 console.log(x,y,z,w,q,r)
 console.log(typeof x,typeof y,typeof z,typeof p,typeof q, typeof r)
+
+
+let o = {
+
+    name : "Guddiya singh",
+    "jobe role": 5600
+}
+
+console.log(o)
+
+o.salary = "100cores",
+console.log(o)
+
+// 1 create a variable of type string and try to add a number to it .
+
+var d = "string"
+console.log(d +4)
+
+
+// 2. Use type of operator to find the datatype of the string in last question
+
+console.log(typeof d , typeof 4);
+
+// 3 Create a const object in javascrpt . Can you change it to hold a number later?
+
+const n ={
+
+    name: "Guddiya",
+    age:21
+};
+
+n.age = 22
+
+
+console.log(n);
+
+// Quick Qug 
+//  Write a javascript program to stroe name , phone number and marks of a student using objects
+
+const student = {
+    name:"GS",
+    "phone number":6678900987,
+    marks : 80
+
+}
+
+//4. Try to add a new key to the const object in problem 3 where you to do it?
+
+student.age = 16;
+
+console.log(student);
+
+// 5. Write a js program to create a word-meaning dectonary of 5 words.
+
+const dec ={
+
+apple: "shyw",
+banana: "kyla",
+guva: "amrudh",
+book:"kithab",
+pen:"klm"
+
+}
