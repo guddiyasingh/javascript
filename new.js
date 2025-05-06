45b0b4b230bd95f065206a238e7c071807e554fd
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-05-06 21:55:59 +0530

    commit

--- new.js
+++ new.js
@@ -5,8 +5,15 @@ console.log("code is looking like a new ...")
 
 var a = 5;
 var b = 6;
+b=4;
 var c = "Harry";
+var _a = "Shubham";
+// var 55a = "Rohan" ; Not Allowed
 
 
 console.log(a + b +8)
-console.log(typeof a, typeof b ,typeof c)
+console.log(typeof a, typeof b ,typeof c);
+
+
+// const a1 = 6;
+// a1 = a1+1; // Not Allowed because a1 is constant
