09d82207b4c6a22cc05b91d400dffcf3b9e44a16
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-05-06 21:36:29 +0530

    var

--- new.js
+++ new.js
@@ -1,3 +1,12 @@
-console.log("code is running...")
+console.log("Hellow World..")
 console.log("code is also running...")
 console.log("code is looking like a new ...")
+
+
+var a = 5;
+var b = 6;
+var c = "Harry";
+
+
+console.log(a + b +8)
+console.log(typeof a, typeof b ,typeof c)
