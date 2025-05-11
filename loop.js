b0a4ec27fafc55d4072d8747870019136d0a8e51
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-05-11 13:52:33 +0530

    in loop

--- loop.js
+++ loop.js
@@ -4,3 +4,17 @@ for (let i=0; i< 100; i++) {
 
     console.log(a+i)
 }
+
+
+let obj = {
+
+    name:"Guddiya",
+    role: "programmer",
+    company:"Google"
+}
+
+for (const key in obj){
+
+    const element = obj[key];
+    console.log(element)
+}
