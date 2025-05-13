334d9a7f1cb47a89d0699557a24f96d0d016f174
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-05-13 11:47:15 +0530

    loop in array

--- Array.js
+++ Array.js
@@ -8,3 +8,27 @@ console.log(arr[0])
 console.log(arr[2])
 console.log(arr[3])
 console.log(arr[4])
+
+console.log(arr.toString())
+console.log(arr.join("and"))
+console.log(arr.pop())
+console.log(arr.push("Harry"))
+console.log(arr.shift())
+console.log(arr.unshift("jack"))
+
+console.log(arr);
+
+console.log(arr.splice(1,2))
+console.log(arr)
+console.log(arr.splice(1, 3, 2222, 333))
+console.log(arr)
+
+//loop in array
+
+let a = [1,2,3,4,5]
+
+for (let index = 0; index < a.length; index++) {
+    const element = a[index];
+    console.log(element)
+    
+}
