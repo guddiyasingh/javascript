b648af78e42cbfd89dd9abe8d2f5480ec122a625
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-05-13 12:19:11 +0530

    for in

--- Array.js
+++ Array.js
@@ -36,3 +36,20 @@ for (let index = 0; index < a.length; index++) {
 a.forEach((value, index , arr) =>{
     console.log(value, index , arr)
 })
+
+
+let obj = {
+
+    a:1,
+    b:2,
+    c:3
+}
+
+for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        const element = obj[key];
+        
+    }
+}
+
+console.log(obj)
