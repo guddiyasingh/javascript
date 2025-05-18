b7452179bfd9d0dbf1752b55caa6f56a1f4241de
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-05-18 22:37:34 +0530

    add some code

--- .vscode/javaScript.js
+++ .vscode/javaScript.js
@@ -3,3 +3,8 @@
 document.querySelector(".container").innerText
 document.querySelector(".container").outerHTML
 document.querySelector(".container").tagName
+
+let div = document.createElement("div");
+div.innerHTML = "I have been inserted <b>by harry </b>"
+div.setAttribute("class","created");
+document.querySelector(".container").before(div);
