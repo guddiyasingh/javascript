37d2c6a71d406a75e05fecf5ca17bc7db207664f
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-05-28 17:47:27 +0530

    add some code

--- promise.js
+++ promise.js
@@ -1,1 +1,13 @@
-console.log('This is promises')
+console.log('This is promises');
+
+
+let prom1 = new Promise((resolve, reject) =>{
+    setTimeout(() =>{
+        console.log("Yes I am done")
+        resolve("Harry")
+    }, 3000);
+})
+
+prom1.then((a)=>{
+    console.log(a)
+})
